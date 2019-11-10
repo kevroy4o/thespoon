64f5369ef30dbf0f3c94acf4f52b6126f6ca4386
Kevin Richardson <richalk@gmail.com>
2019-11-10 11:33:59 +0000

    First

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,21 @@
+class RestaurantsController < ApplicationController
+  RESTAURANTS = {
+    1 => { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
+    2 => { name: "Sushi Samba", address: "City, London", category: "japanese" }
+  }
+
+  def index
+    if params[:food_type].blank?
+      @restaurants = RESTAURANTS
+    else
+    @restaurants = RESTAURANTS.select do |id, restaurant|
+      restaurant[:category] == params[:food_type]
+      end
+    end
+  end
+
+  def create
+
+    puts "TODO: Create the restaurant in the DB"
+  end
+end
